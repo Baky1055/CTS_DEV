ad6fde1f0b73d1dcc078fafdb08422beecd7ad04
Abdullah1055 <mrk3218@gmail.com>
2023-02-16 12:58:04 +0600

    new

--- main.py
+++ main.py
@@ -16,5 +16,13 @@ driver.find_element(By.XPATH, "//button[normalize-space()='Login']").click()
 # driver.find_element(By.XPATH, "// a[normalize - space() = 'Logout']").click()
 time.sleep(2)
 
+dd = driver.find_elements(By.XPATH, "//li[@class='dropdown']//ul[@class='dropdown-menu']")
+
+i = 0
+for i in range(i, dd):
+    if i < dd:
+        logout = driver.find_element(By.XPATH, "//a[normalize-space()='Logout']").click()
+
+time.sleep(2)
 # nav_top_links = driver.find_element(By.ID, "user-img-img-raised")
 # signout = nav_top_links.driver.find_element(By.XPATH, "//a[normalize-space()='Logout']").click()
